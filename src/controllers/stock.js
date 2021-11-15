6955a9dc6dbd1fbf54f1b3b7d6d796aa6a2d52e1
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-11-15 19:01:29 -0300

    chore remove string from mongo request

--- src/controllers/stock.js
+++ src/controllers/stock.js
@@ -12,19 +12,15 @@ function index(req, res) {
 
 function show(req, res) {
   const tickerName = req.params.ticker.toUpperCase();
-  StockModel.findOne(
-    { tickerName },
-    "tickerName companyName tyckerType formattedPrice",
-    (err, data) => {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        data
-          ? res.status(200).json(data)
-          : res.status(404).json({ error: "Not Found a Stock with this name" });
-      }
+  StockModel.findOne({ tickerName }, (err, data) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      data
+        ? res.status(200).json(data)
+        : res.status(404).json({ error: "Not Found a Stock with this name" });
     }
-  );
+  });
 }
 
 export const Stock = {
