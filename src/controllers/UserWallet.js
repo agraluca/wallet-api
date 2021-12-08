271fddd9512f0bab802331bcc6b2697972d3eda7
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-12-08 12:34:46 -0300

    feat: Add fixed Income Wallet requests in UserWallet controllers and routes

--- src/controllers/UserWallet.js
+++ src/controllers/UserWallet.js
@@ -1,250 +1,329 @@
-import UserWalletModel from "../models/UserWallet.js";
+import UserWalletModel from '../models/UserWallet.js'
 
-import {
-  getToken,
-  validateParams,
-  validateParamsArray,
-} from "../utils/index.js";
+import { getToken, validateParams, validateParamsArray } from '../utils/index.js'
 
 export async function getUserWallet(req, res) {
-  const data = getToken(req);
-  try {
-    const userWallet = await UserWalletModel.findOne({ id: data.id }, "-_id");
+    const data = getToken(req)
+    try {
+        const userWallet = await UserWalletModel.findOne({ id: data.id }, '-_id')
 
-    return res.status(200).json({
-      msg: "Carteira carregada com sucesso",
-      userWallet: {
-        id: userWallet.id,
-        wallet: userWallet.wallet,
-        walletFixedIncome: userWallet.walletFixedIncome,
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+        return res.status(200).json({
+            msg: 'Carteira carregada com sucesso',
+            userWallet: {
+                id: userWallet.id,
+                wallet: userWallet.wallet,
+                walletFixedIncome: userWallet.walletFixedIncome,
+            },
+        })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
+}
+
+export async function addStockToFixedIncomeWallet(req, res) {
+    const data = getToken(req)
+
+    const { name, idealPorcentage, currentPorcentage, totalPrice, shouldBuyPrice, status } = req.body
+
+    const paramsSchema = {
+        name: '',
+        idealPorcentage: '',
+        currentPorcentage: '',
+        totalPrice: '',
+        shouldBuyPrice: '',
+        status: '',
+    }
+
+    const errors = validateParams(req.body, paramsSchema)
+
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
+
+    const user = await UserWalletModel.findOne({ id: data.id })
+
+    try {
+        if (user.walletFixedIncome.find((ticker) => ticker.name.toUpperCase() === name.toUpperCase())) {
+            return res.status(422).json({
+                msg: 'Você já possui esse ativo na sua carteira',
+            })
+        }
+
+        user.walletFixedIncome.push({
+            name,
+            idealPorcentage,
+            currentPorcentage,
+            totalPrice,
+            shouldBuyPrice,
+            status,
+        })
+
+        await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome: user.walletFixedIncome })
+        return res.status(200).json({ msg: 'Ativo adicionado com sucesso!' })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ msg: error })
+    }
 }
 
 export async function addStockToWallet(req, res) {
-  const data = getToken(req);
+    const data = getToken(req)
 
-  const {
-    stock,
-    type,
-    companyName,
-    idealPorcentage,
-    currentPorcentage,
-    price,
-    stockAmount,
-    shouldBuyAmount,
-    status,
-  } = req.body;
+    const {
+        stock,
+        type,
+        companyName,
+        idealPorcentage,
+        currentPorcentage,
+        price,
+        stockAmount,
+        shouldBuyAmount,
+        status,
+    } = req.body
 
-  const paramsSchema = {
-    stock: "",
-    type: "",
-    idealPorcentage: "",
-    currentPorcentage: "",
-    price: "",
-    stockAmount: "",
-    shouldBuyAmount: "",
-    status: "",
-  };
-
-  const errors = validateParams(req.body, paramsSchema);
-
-  if (errors) {
-    return res.status(422).json({ msg: errors });
-  }
-
-  const user = await UserWalletModel.findOne({ id: data.id });
-
-  try {
-    if (
-      user.wallet.find(
-        (ticker) => ticker.stock.toUpperCase() === stock.toUpperCase()
-      )
-    ) {
-      return res
-        .status(422)
-        .json({ msg: "Você já possui esse ativo na sua carteira" });
+    const paramsSchema = {
+        stock: '',
+        type: '',
+        idealPorcentage: '',
+        currentPorcentage: '',
+        price: '',
+        stockAmount: '',
+        shouldBuyAmount: '',
+        status: '',
     }
 
-    user.wallet.push({
-      stock,
-      type,
-      companyName,
-      idealPorcentage,
-      currentPorcentage,
-      price,
-      stockAmount,
-      shouldBuyAmount,
-      status,
-    });
+    const errors = validateParams(req.body, paramsSchema)
 
-    await UserWalletModel.updateOne({ id: data.id }, { wallet: user.wallet });
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
 
-    return res.status(200).json({ msg: "Ativo adicionado com sucesso!" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+    const user = await UserWalletModel.findOne({ id: data.id })
+
+    try {
+        if (user.wallet.find((ticker) => ticker.stock.toUpperCase() === stock.toUpperCase())) {
+            return res.status(422).json({
+                msg: 'Você já possui esse ativo na sua carteira',
+            })
+        }
+
+        user.wallet.push({
+            stock,
+            type,
+            companyName,
+            idealPorcentage,
+            currentPorcentage,
+            price,
+            stockAmount,
+            shouldBuyAmount,
+            status,
+        })
+
+        await UserWalletModel.updateOne({ id: data.id }, { wallet: user.wallet })
+
+        return res.status(200).json({ msg: 'Ativo adicionado com sucesso!' })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
+}
+
+export async function updateFixedIncomeWallet(req, res) {
+    const paramsSchema = { walletFixedIncome: [] }
+    const walletFixedIncomeSchema = [
+        'name',
+        'idealPorcentage',
+        'currentPorcentage',
+        'totalPrice',
+        'shouldBuyPrice',
+        'status',
+    ]
+    const errors = validateParams(req.body, paramsSchema)
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
+
+    const data = getToken(req)
+    const { walletFixedIncome } = req.body
+
+    const errorsOnFixedIncomeWallet = validateParamsArray(walletFixedIncome, walletFixedIncomeSchema)
+
+    if (errorsOnFixedIncomeWallet) {
+        return res.status(422).json({ msg: errorsOnWallet })
+    }
+
+    try {
+        await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome })
+        return res.status(200).json({ msg: 'Carteira atualizda com sucesso!' })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
 }
 
 export async function updateWallet(req, res) {
-  const paramsSchema = { wallet: [] };
-  const walletSchema = [
-    "stock",
-    "type",
-    "idealPorcentage",
-    "currentPorcentage",
-    "price",
-    "stockAmount",
-    "shouldBuyAmount",
-    "status",
-  ];
-  const errors = validateParams(req.body, paramsSchema);
-  if (errors) {
-    return res.status(422).json({ msg: errors });
-  }
-  const data = getToken(req);
-  const { wallet } = req.body;
+    const paramsSchema = { wallet: [] }
+    const walletSchema = [
+        'stock',
+        'type',
+        'idealPorcentage',
+        'currentPorcentage',
+        'price',
+        'stockAmount',
+        'shouldBuyAmount',
+        'status',
+    ]
+    const errors = validateParams(req.body, paramsSchema)
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
+    const data = getToken(req)
+    const { wallet } = req.body
 
-  const errorsOnWallet = validateParamsArray(wallet, walletSchema);
+    const errorsOnWallet = validateParamsArray(wallet, walletSchema)
 
-  if (errorsOnWallet) {
-    return res.status(422).json({ msg: errorsOnWallet });
-  }
+    if (errorsOnWallet) {
+        return res.status(422).json({ msg: errorsOnWallet })
+    }
 
-  try {
-    await UserWalletModel.updateOne({ id: data.id }, { wallet });
-    return res.status(200).json({ msg: "Carteira atualizda com sucesso!" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+    try {
+        await UserWalletModel.updateOne({ id: data.id }, { wallet })
+        return res.status(200).json({ msg: 'Carteira atualizda com sucesso!' })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
+}
+
+export async function removeStockFromFixedIncomeWallet(req, res) {
+    const { id } = req.params
+    const paramsSchema = {
+        id: '',
+    }
+    const errors = validateParams(req.params, paramsSchema)
+
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
+
+    const data = getToken(req)
+    const user = await UserWalletModel.findOne({ id: data.id })
+    const walletFixedIncome = user?.walletFixedIncome?.filter((item) => item._id.toString() !== id)
+
+    try {
+        await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome })
+        return res.status(200).json({
+            msg: 'Ativo removido com sucesso!',
+        })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
 }
 
 export async function removeStockFromWallet(req, res) {
-  const { id } = req.params;
+    const { id } = req.params
 
-  const data = getToken(req);
-  const user = await UserWalletModel.findOne({ id: data.id });
-  const wallet = user?.wallet?.filter((item) => item._id.toString() !== id);
+    const data = getToken(req)
+    const user = await UserWalletModel.findOne({ id: data.id })
+    const wallet = user?.wallet?.filter((item) => item._id.toString() !== id)
 
-  try {
-    await UserWalletModel.updateOne({ id: data.id }, { wallet });
-    return res.status(200).json({
-      msg: "Ativo removido com sucesso!",
-    });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+    try {
+        await UserWalletModel.updateOne({ id: data.id }, { wallet })
+        return res.status(200).json({
+            msg: 'Ativo removido com sucesso!',
+        })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
 }
 
 export async function addFixedIncomeToWallet(req, res) {
-  const data = getToken(req);
+    const data = getToken(req)
 
-  const {
-    name,
-    idealPorcentage,
-    currentPorcentage,
-    totalPrice,
-    shouldBuyPrice,
-    status,
-  } = req.body;
+    const { name, idealPorcentage, currentPorcentage, totalPrice, shouldBuyPrice, status } = req.body
 
-  const paramsSchema = {
-    name: "",
-    idealPorcentage: "",
-    currentPorcentage: "",
-    totalPrice: "",
-    shouldBuyPrice: "",
-    status: "",
-  };
-
-  const errors = validateParams(req.body, paramsSchema);
-
-  if (errors) {
-    return res.status(422).json({ msg: errors });
-  }
-
-  const user = await UserWalletModel.findOne({ id: data.id });
-
-  try {
-    if (
-      user.walletFixedIncome.find(
-        (item) => item.name.toUpperCase() === name.toUpperCase()
-      )
-    ) {
-      return res
-        .status(422)
-        .json({ msg: "Você já possui esse ativo na sua carteira" });
+    const paramsSchema = {
+        name: '',
+        idealPorcentage: '',
+        currentPorcentage: '',
+        totalPrice: '',
+        shouldBuyPrice: '',
+        status: '',
     }
 
-    user.walletFixedIncome.push({
-      name,
-      idealPorcentage,
-      currentPorcentage,
-      totalPrice,
-      shouldBuyPrice,
-      status,
-    });
+    const errors = validateParams(req.body, paramsSchema)
 
-    await UserWalletModel.updateOne(
-      { id: data.id },
-      { walletFixedIncome: user.walletFixedIncome }
-    );
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
 
-    return res.status(200).json({ msg: "Renda fixa adicionada com sucesso!" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+    const user = await UserWalletModel.findOne({ id: data.id })
+
+    try {
+        if (user.walletFixedIncome.find((item) => item.name.toUpperCase() === name.toUpperCase())) {
+            return res.status(422).json({
+                msg: 'Você já possui esse ativo na sua carteira',
+            })
+        }
+
+        user.walletFixedIncome.push({
+            name,
+            idealPorcentage,
+            currentPorcentage,
+            totalPrice,
+            shouldBuyPrice,
+            status,
+        })
+
+        await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome: user.walletFixedIncome })
+
+        return res.status(200).json({ msg: 'Renda fixa adicionada com sucesso!' })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
 }
 
 export async function updateFixedIncomeToWallet(req, res) {
-  const paramsSchema = { walletFixedIncome: [] };
-  const walletSchema = [
-    "name",
-    "idealPorcentage",
-    "currentPorcentage",
-    "totalPrice",
-    "shouldBuyPrice",
-    "status",
-  ];
-  const errors = validateParams(req.body, paramsSchema);
-  if (errors) {
-    return res.status(422).json({ msg: errors });
-  }
-  const data = getToken(req);
-  const { walletFixedIncome } = req.body;
+    const paramsSchema = { walletFixedIncome: [] }
+    const walletSchema = [
+        'name',
+        'idealPorcentage',
+        'currentPorcentage',
+        'totalPrice',
+        'shouldBuyPrice',
+        'status',
+    ]
+    const errors = validateParams(req.body, paramsSchema)
+    if (errors) {
+        return res.status(422).json({ msg: errors })
+    }
+    const data = getToken(req)
+    const { walletFixedIncome } = req.body
 
-  const errorsOnWallet = validateParamsArray(walletFixedIncome, walletSchema);
+    const errorsOnWallet = validateParamsArray(walletFixedIncome, walletSchema)
 
-  if (errorsOnWallet) {
-    return res.status(422).json({ msg: errorsOnWallet });
-  }
+    if (errorsOnWallet) {
+        return res.status(422).json({ msg: errorsOnWallet })
+    }
 
-  try {
-    await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome });
-    return res.status(200).json({ msg: "Carteira atualizda com sucesso!" });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+    try {
+        await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome })
+        return res.status(200).json({ msg: 'Carteira atualizda com sucesso!' })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
 }
 
 export async function removeFixedIncomeFromWallet(req, res) {
-  const { id } = req.params;
+    const { id } = req.params
 
-  const data = getToken(req);
-  const user = await UserWalletModel.findOne({ id: data.id });
-  const walletFixedIncome = user?.walletFixedIncome?.filter(
-    (item) => item._id.toString() !== id
-  );
+    const data = getToken(req)
+    const user = await UserWalletModel.findOne({ id: data.id })
+    const walletFixedIncome = user?.walletFixedIncome?.filter((item) => item._id.toString() !== id)
 
-  try {
-    await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome });
-    return res.status(200).json({
-      msg: "Ativo removido com sucesso!",
-    });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+    try {
+        await UserWalletModel.updateOne({ id: data.id }, { walletFixedIncome })
+        return res.status(200).json({
+            msg: 'Ativo removido com sucesso!',
+        })
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,51 +1,28 @@
-import express from "express";
+import express from 'express'
 
-import * as UserController from "../controllers/User.js";
-import * as controllers from "../controllers/index.js";
-import * as UserWalletController from "../controllers/UserWallet.js";
+import * as UserController from '../controllers/User.js'
+import * as controllers from '../controllers/index.js'
+import * as UserWalletController from '../controllers/UserWallet.js'
 
-import { checkToken } from "../utils/index.js";
+import { checkToken } from '../utils/index.js'
 
-const routes = express.Router();
+const routes = express.Router()
+//! Auth
+routes.post('/auth/register', UserController.createUser)
+routes.post('/auth/signin', UserController.sigInUser)
+routes.post('/auth/refresh', checkToken, UserController.refreshToken)
+//! Stock
+routes.get('/stock/:ticker', checkToken, controllers.Stock.show)
+//! Wallet
+routes.get('/wallet/get', checkToken, UserWalletController.getUserWallet)
+routes.post('/wallet/add', checkToken, UserWalletController.addStockToWallet)
+routes.put('/wallet/update', checkToken, UserWalletController.updateWallet)
+routes.delete('/wallet/remove/:id', checkToken, UserWalletController.removeStockFromWallet)
+//! FixedIncomeWallet
+routes.post('/walletFixedIncome/add', checkToken, UserWalletController.addFixedIncomeToWallet)
+routes.put('/walletFixedIncome/update', checkToken, UserWalletController.updateFixedIncomeToWallet)
+routes.delete('/walletFixedIncome/remove/:id', checkToken, UserWalletController.removeFixedIncomeFromWallet)
 
-routes.post("/auth/register", UserController.createUser);
+routes.get('/', (req, res) => res.send('Wallet API'))
 
-routes.post("/auth/signin", UserController.sigInUser);
-
-routes.post("/auth/refresh", checkToken, UserController.refreshToken);
-
-routes.get("/stock/:ticker", checkToken, controllers.Stock.show);
-
-routes.get("/wallet/get", checkToken, UserWalletController.getUserWallet);
-
-routes.post("/wallet/add", checkToken, UserWalletController.addStockToWallet);
-
-routes.put("/wallet/update", checkToken, UserWalletController.updateWallet);
-
-routes.delete(
-  "/wallet/remove/:id",
-  checkToken,
-  UserWalletController.removeStockFromWallet
-);
-
-routes.post(
-  "/walletFixedIncome/add",
-  checkToken,
-  UserWalletController.addFixedIncomeToWallet
-);
-
-routes.put(
-  "/walletFixedIncome/update",
-  checkToken,
-  UserWalletController.updateFixedIncomeToWallet
-);
-
-routes.delete(
-  "/walletFixedIncome/remove/:id",
-  checkToken,
-  UserWalletController.removeFixedIncomeFromWallet
-);
-
-routes.get("/", (req, res) => res.send("Wallet API"));
-
-export default routes;
+export default routes
